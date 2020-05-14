1f12afa83469d6019007470e6721dcea123de0a4
qjabc <1677006753@qq.com>
2020-05-14 19:09:49 +0800

    modified lex_yacc.h

--- LexYacc/lex_yacc.h
+++ LexYacc/lex_yacc.h
@@ -19,3 +19,20 @@ void parse_file(const std::string &filename) {
 	printf("parse end\n");
 	fclose(file);
 }
+
+void parse_string(const std::string &s) {
+	printf("parse string \"%s\"\n", s.c_str());
+	FILE *file = fopen("json.txt", "w");
+	if (file == NULL) {
+		printf("cannot open \"json.txt\"\n");
+		exit(1);
+	}
+	fputs(s.c_str(), file);
+	fseek(file, 0, SEEK_SET);
+	extern FILE *yyin;
+	yyin = file;
+	printf("parse begin\n");
+	yyparse();
+	printf("parse end\n");
+	fclose(file);
+}
